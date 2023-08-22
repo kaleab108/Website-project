09f7fb4f3c0cba87f88433017c45d9172aeb26f4
Kaleab Yakob <78443389+kaleab108@users.noreply.github.com>
2023-08-21 23:05:42 -0700

    InnerHTML Error

---
+++ app.js
@@ -0,0 +1,51 @@
+const filterButton = document.querySelector('#filter-button');
+const keywordsInput = document.querySelector('#keywords');
+const locationInput = document.querySelector('#location')
+const featuredJobsContainer = document.querySelector('.feautred-jobs');
+
+filterButton.addEventListener('click', event => {
+    event.preventDefault();
+
+    const keywords = keywordsInput.value.trim().toLowerCase();
+    const location = locationInput.value.trim().toLowerCase();
+
+    featuredJobsContainer.innerHTML = '';
+
+    fetch('/path/to/jobs-data.json')
+        .then(response => response.json())
+        .then(jobs => {
+            // Filter the jobs by keywords and location
+            const filteredJobs = jobs.filter(job => {
+                return (job.title.toLowerCase().includes(keywords) && job.location.toLowerCase().includes(location)
+                );
+            });
+            filteredJobs.forEach(generateJobElement);
+        });
+});
+
+function generateJobElement(job) {
+
+    const jobELement = document.createElement('div');
+    const titleElement = document.createElement('h3');
+    const companyElement = document.createElement('p');
+    const locationElement = document.createElement('p');
+    const descriptionElement = document.createElement('p');
+    const applyButton = document.createElement('a');
+
+    titleElement.textContent = job.title;
+    companyElement.textContent = job.company;
+    locationElement.textContent = job.location;
+    descriptionElement.textContent = job.description;
+    applyButton.textContent = 'Apply Now';
+    applyButton.href = job.apply_url;
+
+    //append the elemnts to the job element
+
+    jobELement.appendChild(titleElement);
+    jobELement.appendChild(companyElement);
+    jobELement.appendChild(locationElement);
+    jobELement.appendChild(descriptionElement);
+    jobELement.appendChild(applyButton);
+
+    featuredJobsContainer.appendChild(jobELement);
+}
